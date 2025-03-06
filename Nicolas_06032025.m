6a469c026efe7a580ce663b91d20a956e3a43470
patchkrz <59878633+patchkrz@users.noreply.github.com>
2025-03-06 16:18:24 +0100

    Add files via upload
    Comments are added. (This version omits the NaN values in static electric field vector.)

--- Nicolas_06032025.m
+++ Nicolas_06032025.m
@@ -1,3 +1,29 @@
+%% -----
+% 
+% This code computes field overlap for optical E-field and the E-field
+% modulating it. 
+%
+% * The inputs of this code are optical E-field, modulating E-field,
+% electric potential and effective refractive index which is computed at
+% the points user defines.
+% 
+%
+% --- Data Export Order ---
+% This is important to be able to keep track of data. The following order
+% is preferred on COMSOL export interface:
+%
+% ewfd.Ex [V/m] -> Optical Field x-component
+% ewfd.Ez [V/m] -> Optical Field z-component
+% es.Ex [V/m]   -> Static Field x-component
+% V             -> Electric Potential
+% ewfd.neff     -> Effective mode index
+%
+%
+% Results and comparisions:
+% https://docs.google.com/spreadsheets/d/11hWJHqptUex4R8OjKD7x7_AgD8aYe10y9XUeD_9BMqI/edit?usp=sharing
+%
+% -----
+
 data = load('nicolas_06032025_field_amplitudes.txt');
 
 amp_ewfdEx = data(:,23);
@@ -11,30 +37,21 @@ filtered_ewfdEx = amp_ewfdEx(validData);
 filtered_ewfdEz = amp_ewfdEz(validData);
 filtered_esEx = amp_esEx(validData);  
 
-% --- Coordinate values ---
-% It is the crossection area of the system. Simulator calls dimensions as x
-% and y-coordinates.    
-% x = data(:,1);
-% y = data(:,2);
-% 
-% x = x(validData); 
-% y = y(validData); 
+% E = fillmissing(amp_esEx, 'linear');
 
 % meshgrid and multidimensional integration preparetion
 [Ex,Ez] = meshgrid(filtered_ewfdEx,filtered_ewfdEz);
 ewdfE = sqrt(Ex.^2 + Ez.^2);
 
-n_eff = 1.9286; % effective refractive index
-v_dc = 1; % control voltage
+n_eff = 1.9286; % effective refractive index (from COMSOL)
+v_dc = 1; % control voltage (from COMSOL)
 g = 3.2e-6; % gap between signal tracks 
 lam = 1055e-9; % operating wavelength
 ne = 2.156; % extraordinary refractive index of LN at 1064nm
 r33 = 30.8e-12; % [meter/Volt] (Yariv) EO coefficient
-L = 1e-2;
+L = 4e-3;
 
 nom_overlap = g * trapz(filtered_ewfdEx, trapz(filtered_ewfdEz,(ewdfE.^2).*filtered_esEx,1));
-% deneme = trapz(filtered_ewfdEx,(ewdfE.^2).*filtered_esEx,1);
-% deneme_devam = g * trapz(filtered_ewfdEz, deneme);
 
 denom_overlap = v_dc * trapz(filtered_ewfdEx,trapz(filtered_ewfdEz,(ewdfE.^2),1));
  
